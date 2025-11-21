01fc08068af5c1c13e8ed2380566d5d4ff150a31
S Vyaas Sundar <115569294+svscodes@users.noreply.github.com>
2025-11-21 22:07:22 +0530

    Update solution: 34. Find First and Last Position of Element in Sorted Array

--- Easy/find-first-and-last-position-of-element-in-sorted-array.py
+++ Easy/find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,47 +6,66 @@
 # Date: 2025-11-21
 
 
-        #             start = mid
-        #         else:
-        #             start = self.first(nums,target)
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # left = 0
+        # right = len(nums)-1
+        # start = -1
+        # end = -1
 
-        
-        #         if nums[mid+1] != target:
-        #             end = mid
-        #         else:
-        #             end = self.last(nums,target)
+        # def first(self,nums,target){
+        #     idx = -1
+        #     low,high = 0,len(nums)-1
+
+        #     while low<=high:
+        #         mid = lo
+
+        # }
+
+        # def last(self,nums,target){
+
+        # }
+
+        # while left < right:
+        #     mid = (left+right)//2
+        #     if nums[mid] == target:
+        #         if nums[mid-1] != target:
+        #             start = mid
+        #         else:
+        #             start = self.first(nums,target)
+
+        
+        #         if nums[mid+1] != target:
+        #             end = mid
+        #         else:
+        #             end = self.last(nums,target)
 
 
-        #     elif nums[mid] < target:
-        #         left = mid+1
-        #     else:
-        #         right = mid-1
+        #     elif nums[mid] < target:
+        #         left = mid+1
+        #     else:
+        #         right = mid-1
 
-                 
-        # return [start,end]
-        
-        #         if nums[mid-1] != target:
-        #     if nums[mid] == target:
-        # while left < right:
-        #     mid = (left+right)//2
-        # }
+                 
+        # return [start,end]
+        
+        def search(x):
+            lo, hi = 0, len(nums)           
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if nums[mid] < x:
+                    lo = mid+1
+                else:
+                    hi = mid                    
+            return lo
+        
+        lo = search(target)
+        hi = search(target+1)-1
+        
+        if lo <= hi:
+            return [lo, hi]
+                
+        return [-1, -1]
 
-        # def last(self,nums,target){
 
-        # }
-
-        #         mid = lo
-
-
-        #     while low<=high:
-        #     low,high = 0,len(nums)-1
-        #     idx = -1
-        # def first(self,nums,target){
-
-        # end = -1
-        # start = -1
-        # right = len(nums)-1
-        # left = 0
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-class Solution:
-
+        
